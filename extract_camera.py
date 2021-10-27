531f6263af028490769c988ba552b040d5ce7f79
Martin Ahrnbom <martin.ahrnbom@math.lth.se>
2021-10-27 16:11:01 +0200

    started on scenarios

---
+++ extract_camera.py
@@ -0,0 +1,35 @@
+"""
+    Simple script to get the position and orientation of camera written to the terminal. 
+    This script was used to create the default scenarios.
+"""
+
+import carla
+import argparse
+import time 
+
+from stsvdc import loc_dist
+
+host = '127.0.0.1'
+port = 2000
+client = carla.Client(host, port)
+client.set_timeout(10.0)
+
+args = argparse.ArgumentParser()
+args.add_argument("--map", type=str, default="Town10")
+args = args.parse_args()
+map_name = args.map
+
+client.load_world(map_name)
+time.sleep(1.0)
+
+world = client.get_world()
+
+old_pos = None 
+def_length = 2000
+while True:
+    transform = world.get_spectator().get_transform()
+    pos = transform.location
+    rot = transform.rotation
+    if old_pos and loc_dist(pos, old_pos) > 0.001:
+        print(f"Scenario(map='{map_name}', cam_pos=({pos.x},{pos.y},{pos.z}), cam_dir=({rot.pitch},{rot.yaw},{rot.roll}), length={def_length})")
+    old_pos = pos
--- stsvdc.py
+++ stsvdc.py
@@ -7,18 +7,45 @@ from pathlib import Path
 import sys
 import random
 import time 
+from dataclasses import dataclass
 
 import carla
 from carla import VehicleLightState as vls
 
 def loc_dist(a, b):
     return (a.x - b.x)**2 + (a.y - b.y)**2 + (a.z - b.z)**2
-    
 
-def main(host:str, port:int, tm_port:int):
+@dataclass
+class Scenario:
+    map:str
+    cam_pos:tuple
+    cam_dir:tuple
+    length:int
+
+def default_scenarios():
+    scenrarios = list()
+    s = Scenario(map='Town01', cam_pos=(101.38188171386719,183.1450958251953,7.839078903198242), cam_dir=(-24.54534149169922,130.83407592773438,4.599098247126676e-05), length=2000)
+    scenrarios.append(s)
+
+    s = Scenario(map='Town01', cam_pos=(323.6024475097656,185.66769409179688,10.069890975952148), cam_dir=(-32.938194274902344,55.12174987792969,3.051888779737055e-05), length=2000)
+    scenrarios.append(s)
+
+    s = Scenario(map='Town10HD', cam_pos=(-51.66246032714844,154.75738525390625,13.823832511901855), cam_dir=(-27.47414779663086,-86.13775634765625,0.00011643866309896111), length=2000)
+    scenrarios.append(s)
+
+    return scenrarios
+
+def main(host:str, port:int, tm_port:int, cam_setup:list):
     client = carla.Client(host, port)
     client.set_timeout(10.0)
-    
+    traffic_manager = client.get_trafficmanager(tm_port)
+
+    scenarios = default_scenarios()
+    for s_num, scenario in enumerate(scenarios):
+        print(f"Starting scenario {s_num+1} / {len(scenarios)}")
+        run_scenario(client, traffic_manager, scenario)
+
+def run_scenario(client, traffic_manager, scenario:Scenario):
     # Some parameters
     number_of_vehicles=80
     number_of_pedestrians=42
@@ -29,7 +56,6 @@ def main(host:str, port:int, tm_port:int):
 
     # Setup world and traffic manager
     world = client.get_world()
-    traffic_manager = client.get_trafficmanager(tm_port)
     traffic_manager.set_global_distance_to_leading_vehicle(1.0)
     traffic_manager.set_random_device_seed(seed)
 
@@ -154,9 +180,15 @@ def main(host:str, port:int, tm_port:int):
 
     traffic_manager.global_percentage_speed_difference(30.0)
 
-    while True:
+    print("Stabilizing world...")
+    for _ in range(100):
         world.tick()
         time.sleep(0.005)
+
+    print("World stabilized")
+
+    # Spawn camera(s)
+    
 
 
 
@@ -166,10 +198,11 @@ if __name__ == '__main__':
     args.add_argument("--host", default='127.0.0.1', help="IP of the Carla server host")
     args.add_argument("--port", default=2000, help="Port to connect to the server", type=int)
     args.add_argument("--tm_port", default=8000, type=int, help="Traffic Manager communications port")
+    args.add_argument("--cam_setup", default=[(0,0,0)], help="List of camera offsets. To make a stereo camera setup with 0.5 metres distance, do [(0,0,0), (0.5,0,0)]. Each tuple contains x, y and z distances from the default camera position. The camera is always facing in positive z direction. ")
     args = args.parse_args()
 
     try:
-        main(host=args.host, port=args.port, tm_port=args.tm_port)
+        main(host=args.host, port=args.port, tm_port=args.tm_port, cam_setup=args.cam_setup)
     except KeyboardInterrupt:
         pass
     except:
