01220191f7bf401ee487f40271a2eff2d792f22e
Martin Ahrnbom <martin.ahrnbom@math.lth.se>
2021-11-09 08:59:00 +0100

    start.py now accepts a range

--- start.py
+++ start.py
@@ -9,7 +9,10 @@ def main(args):
     cam_setup = args.cam_setup
     folder = args.folder 
 
-    for s_num in range(args.scenarios):
+    scenario_string = args.range
+    start, stop = [int(v) for v in scenario_string.split('-')]
+
+    for s_num in range(start, stop+1):
         command = [sys.executable, 'stsvdc.py', '--host', host, '--port', port, '--tm_port', tm_port, '--cam_setup', cam_setup, '--folder', folder, '--scenario_number', str(s_num)]
         print(command)
         p = subprocess.Popen(command, shell=False)
@@ -27,7 +30,7 @@ if __name__ == "__main__":
     args.add_argument("--tm_port", default='8000', help="Traffic Manager communications port")
     args.add_argument("--cam_setup", default='0,0,0', help="List of camera offsets. To make a stereo camera setup with 0.5 metres distance, do '0,0,0;0.5,0,0'. Each tuple contains x, y and z distances from the default camera position. The camera is always facing in positive z direction. Separate by semicolons (note that you probably need quotes around this argument).")
     args.add_argument("--folder", default="./output", type=str, help="Folder to store output (default is './output')")
-    args.add_argument("--scenarios", default=20, type=int, help="Number of scenarios to run")
+    args.add_argument("--range", default='0-20', type=str, help="Which scenarios to run, as two integers separated by a dash, like '0-20' (inclusive)")
 
     args = args.parse_args()
     main(args)
